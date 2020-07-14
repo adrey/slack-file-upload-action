ad2ef4a330dd7000e69e2d9aff9b935ef21c53f0
adray <avsafronof@gmail.com>
2020-07-14 15:49:15 +0300

    Wrap with promise

--- dist/index.js
+++ dist/index.js
@@ -44,7 +44,7 @@ module.exports =
 /******/ ({
 
 /***/ 12:
-/***/ (function(module, __unusedexports, __webpack_require__) {
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
 const core = __webpack_require__(572);
 const github = __webpack_require__(558);
@@ -58,53 +58,59 @@ function finishWithError(message) {
     core.setFailed(message);
 }
 
-function processResponse(err, res) {
-console.log(res.statusCode);
-    var data = "";
-    res.resume();
-    res.on("data", (chunk) => {
-        data += chunk;
-    });
-    res.on("error", (e) => {
-        finishWithError(e.message);
-    });
-    res.on("end", () => {
-        data = JSON.parse(data);
-        if(!data.ok) {
-            finishWithError(data.message);
-        }
-    });
-    
-}
+
 
 function submit(form) {
-    form.submit("https://slack.com/api/files.upload", processResponse);
+    return new Promise((resolve, reject) => {
+        var data = "";
+        form.submit("https://slack.com/api/files.upload", (err, res) => {
+            if(err) {
+                reject(err);
+            } else {
+                res.on("data", (chunk) => { data += chunk;});
+                res.on("end", () => {
+                    data = JSON.parse(data);
+                    console.log(data);
+                    if(data.ok) {
+                        resolve(data);
+                    } else {
+                        reject(data.error);
+                    }
+                });
+            }
+        });
+
+
+    });
 }
 
-try {
-    const token = core.getInput('token');
-    const path = core.getInput('path');
-    const channel = core.getInput('channel');
-    const filename = core.getInput('filename');
-    const filetype = core.getInput('filetype');
-    const initial_comment = core.getInput('initial_comment');
-    const thread_ts = core.getInput('thread_ts');
-    const title = core.getInput('title');
-    var form = new FormData();
-    form.append('token', token);
-    form.append('file', fs.createReadStream(path));
-    if(filename) form.append('filename', filename);
-    if(channel) form.append('channels', channel);
-    if(filetype) form.append('filetype', filetype);
-    if(initial_comment) form.append('initial_comment', initial_comment);
-    if(thread_ts) form.append('thread_ts', thread_ts);
-    if(title) form.append('title', title);
-    submit(form);
-} catch (error) {
-    finishWithError(error.message);
+async function run() {
+    try {
+        const token = core.getInput('token');
+        const path = core.getInput('path');
+        const channel = core.getInput('channel');
+        const filename = core.getInput('filename');
+        const filetype = core.getInput('filetype');
+        const initial_comment = core.getInput('initial_comment');
+        const thread_ts = core.getInput('thread_ts');
+        const title = core.getInput('title');
+        var form = new FormData();
+        form.append('token', token);
+        form.append('file', fs.createReadStream(path));
+        if(filename) form.append('filename', filename);
+        if(channel) form.append('channels', channel);
+        if(filetype) form.append('filetype', filetype);
+        if(initial_comment) form.append('initial_comment', initial_comment);
+        if(thread_ts) form.append('thread_ts', thread_ts);
+        if(title) form.append('title', title);
+        await submit(form);
+    } catch (error) {
+        finishWithError(error.message);
+    }
+
 }
 
-module.exports = { processResponse : processResponse, submit : submit }
+run()
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -10,50 +10,56 @@ function finishWithError(message) {
     core.setFailed(message);
 }
 
-function processResponse(err, res) {
-console.log(res.statusCode);
-    var data = "";
-    res.resume();
-    res.on("data", (chunk) => {
-        data += chunk;
-    });
-    res.on("error", (e) => {
-        finishWithError(e.message);
-    });
-    res.on("end", () => {
-        data = JSON.parse(data);
-        if(!data.ok) {
-            finishWithError(data.message);
-        }
-    });
-    
-}
+
 
 function submit(form) {
-    form.submit("https://slack.com/api/files.upload", processResponse);
+    return new Promise((resolve, reject) => {
+        var data = "";
+        form.submit("https://slack.com/api/files.upload", (err, res) => {
+            if(err) {
+                reject(err);
+            } else {
+                res.on("data", (chunk) => { data += chunk;});
+                res.on("end", () => {
+                    data = JSON.parse(data);
+                    console.log(data);
+                    if(data.ok) {
+                        resolve(data);
+                    } else {
+                        reject(data.error);
+                    }
+                });
+            }
+        });
+
+
+    });
 }
 
-try {
-    const token = core.getInput('token');
-    const path = core.getInput('path');
-    const channel = core.getInput('channel');
-    const filename = core.getInput('filename');
-    const filetype = core.getInput('filetype');
-    const initial_comment = core.getInput('initial_comment');
-    const thread_ts = core.getInput('thread_ts');
-    const title = core.getInput('title');
-    var form = new FormData();
-    form.append('token', token);
-    form.append('file', fs.createReadStream(path));
-    if(filename) form.append('filename', filename);
-    if(channel) form.append('channels', channel);
-    if(filetype) form.append('filetype', filetype);
-    if(initial_comment) form.append('initial_comment', initial_comment);
-    if(thread_ts) form.append('thread_ts', thread_ts);
-    if(title) form.append('title', title);
-    submit(form);
-} catch (error) {
-    finishWithError(error.message);
+async function run() {
+    try {
+        const token = core.getInput('token');
+        const path = core.getInput('path');
+        const channel = core.getInput('channel');
+        const filename = core.getInput('filename');
+        const filetype = core.getInput('filetype');
+        const initial_comment = core.getInput('initial_comment');
+        const thread_ts = core.getInput('thread_ts');
+        const title = core.getInput('title');
+        var form = new FormData();
+        form.append('token', token);
+        form.append('file', fs.createReadStream(path));
+        if(filename) form.append('filename', filename);
+        if(channel) form.append('channels', channel);
+        if(filetype) form.append('filetype', filetype);
+        if(initial_comment) form.append('initial_comment', initial_comment);
+        if(thread_ts) form.append('thread_ts', thread_ts);
+        if(title) form.append('title', title);
+        await submit(form);
+    } catch (error) {
+        finishWithError(error.message);
+    }
+
 }
 
-module.exports = { processResponse : processResponse, submit : submit }
+run()
